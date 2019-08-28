c8782bb14cedae28d0d40017dd665667e4d416ec
wangqiang <seeman1978@163.com>
2019-08-28 15:06:44 +0800

    增加hystrix-dashboard 增加hystrix.stream

--- src/main/java/com/example/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
+++ src/main/java/com/example/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MicroserviceSimpleConsumerMovieApplication {
     @Bean
     @LoadBalanced
